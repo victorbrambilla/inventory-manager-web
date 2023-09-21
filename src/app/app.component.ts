f219c0cc1ee4200abe2506f8de2920045f04a12e
victor brambilla <victor-brambilla@hotmail.com>
2023-09-21 17:55:21 -0400

    feat: config layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { GET_STOCKS } from 'src/app/graphql/graphql.queries';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements  OnInit {
+export class AppComponent {
   title = 'inventor-manager-web';
-  constructor(private apollo: Apollo) { }
-  ngOnInit() {
-    this.apollo.watchQuery({
-      query: GET_STOCKS
-    }).valueChanges.subscribe(result => {
-      console.log(result);
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StockTableComponent } from './stock-table/stock-table.component';
+import { FoodTableComponent } from './food-table/food-table.component';
+import { EntryTableComponent } from './entry-table/entry-table.component';
+import { ExitTableComponent } from './exit-table/exit-table.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { NgIf } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockTableComponent,
+    FoodTableComponent,
+    EntryTableComponent,
+    ExitTableComponent,
+    DashboardComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSortModule,
+    NgIf,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
